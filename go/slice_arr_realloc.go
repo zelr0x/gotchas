e00116dd98b33d5659b908b3b0bdf0cf821f22aa
zelr0x <38295414+zelr0x@users.noreply.github.com>
2021-07-25 03:01:45 +0400

    Update slice_arr_realloc.go

--- go/slice_arr_realloc.go
+++ go/slice_arr_realloc.go
@@ -18,6 +18,12 @@ x = 100,	y = 100,	z = 5000
 4
 4
 x = 5000,	y = 5000,	z = 5000
+--- wtf2 ---
+5
+[2 3 4 5]
+5
+5
+x = 5000,	y = 5000,	z = 5000
 */
 func main() {
 	fmt.Println("--- kinda ok ---")
@@ -25,17 +31,29 @@ func main() {
 	fmt.Println()
 	fmt.Println("--- wtf ---")
 	wtf()
+	fmt.Println("--- wtf2 ---")
+	wtf2()
 }
 
-// This one behaves as expected in go.
 func kindaOk() {
 	test(1, 5)
 }
 
-// This one shows that capacity of a slice may be greater
-// than its length even before any appends!
 func wtf() {
 	test(1, 3)
+}
+
+func wtf2() {
+	x := [...]int{1, 2, 3, 4, 5, 6}
+	y := x[1:5]
+	fmt.Println(cap(y))
+	fmt.Println(y)
+	y[0] = 100
+	fmt.Println(cap(y))
+	z := append(y, 1000)
+	fmt.Println(cap(z))
+	z[0] = 5000
+	fmt.Printf("x = %v,\ty = %v,\tz = %v\n", x[1], y[0], z[0])
 }
 
 func test(begin, end uintptr) {
